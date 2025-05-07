44eae05060b715baf812a570ab0c07f43f585005
Richard <richardgms001@gmail.com>
2025-05-07 15:42:16 -0300

    mudanças finais da página, troca de estilo de acordo com horário, inegrando o js ao css.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const img = document.querySelector("#imagem img");
-    const horaDiv = document.querySelector("#hora");
+    const horaDiv = document.querySelector("#hora h2");
+    const titulo = document.querySelector("h1");
     const body = document.body;
 
     function atualizarImagem() {
@@ -10,26 +11,38 @@ document.addEventListener("DOMContentLoaded", function () {
             img.src = "assets/nascerdosol.jpg";
             img.alt = "Nascer do Sol";
             body.className = "nascerdosol";
+            horaDiv.style.color = "#555";
+            titulo.style.color = "#333";
         } else if (hora >= 7 && hora < 12) {
             img.src = "assets/manha.jpg";
             img.alt = "Manhã";
             body.className = "manha";
+            horaDiv.style.color = "#555";
+            titulo.style.color = "#333";
         } else if (hora >= 12 && hora < 17) {
             img.src = "assets/tarde.png";
             img.alt = "Tarde";
             body.className = "tarde";
+            horaDiv.style.color = "#555";
+            titulo.style.color = "#333";
         } else if (hora >= 17 && hora < 19) {
             img.src = "assets/pordosol.jpg";
             img.alt = "Pôr do Sol";
             body.className = "pordosol";
+            horaDiv.style.color = "#555";
+            titulo.style.color = "#333";
         } else if (hora >= 19 || hora < 5) {
             img.src = "assets/noite.png";
             img.alt = "Noite";
             body.className = "noite";
-        } else {
+            horaDiv.style.color = "#fff";
+            titulo.style.color = "#fff";
+        } else if (hora >= 0 && hora < 5) {
             img.src = "assets/madrugada.png";
             img.alt = "Madrugada";
             body.className = "madrugada";
+            horaDiv.style.color = "#fff";
+            titulo.style.color = "#fff";
         }
     }
 
